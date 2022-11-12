bb373c39a208bb8e80b6a12d2596c44e4fe26c49
Aep <kaka.aep.saepudin@gmail.com>
2022-11-12 17:22:46 +0700

    add func winner

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"strings"
 	"sync"
 	"time"
 )
@@ -27,7 +28,7 @@ func rollDice() int {
 }
 
 func main() {
-	setGame(3, 4)
+	setGame(9, 10)
 }
 
 func toCharStr(i int) string {
@@ -37,22 +38,27 @@ func toCharStr(i int) string {
 func setGame(pemain, dadu int) {
 	var (
 		rounde int
-		score  int
-		// player []listPlayer
 	)
 
 	fmt.Println("Pemain = ", pemain, ", Dadu = ", dadu)
 	player := setDice(pemain, dadu)
 	for {
-		rounde++
 		// start Gamae
+		if rounde != 0 {
+			player = setDiceNext(player)
+		}
+		rounde++
 
 		fmt.Println("================================================")
 		fmt.Println("Giliran ", rounde, " lempar dadu:")
 		for _, v := range player {
-			fmt.Print("       Pemain #", v.Player, " (", score, "): ")
-			for _, z := range v.Dice {
-				fmt.Print(" ", z)
+			fmt.Print("       Pemain #", v.Player, " (", v.Score, "): ")
+			if len(v.Dice) != 0 {
+				for _, z := range v.Dice {
+					fmt.Print(" ", z)
+				}
+			} else {
+				fmt.Print("_ (Berhenti bermain karena tidak memiliki dadu)")
 			}
 			fmt.Println("")
 
@@ -61,14 +67,19 @@ func setGame(pemain, dadu int) {
 		fmt.Println("Setelah evaluasi rounde : ", rounde)
 		for _, v := range resEvalu {
 			fmt.Print("       Pemain #", v.Player, " (", v.Score, "): ")
-			for _, z := range v.Dice {
-				fmt.Print(" ", z)
+			if len(v.Dice) != 0 {
+				for _, z := range v.Dice {
+					fmt.Print(" ", z)
+				}
+			} else {
+				fmt.Print("_ (Berhenti bermain karena tidak memiliki dadu)")
 			}
 			fmt.Println("")
-
 		}
 		player = resEvalu
-		if rounde > 20 {
+		if findWinner(resEvalu) {
+
+			fmt.Println("================================================")
 			break
 		}
 	}
@@ -91,43 +102,90 @@ func setDice(pemain, dadu int) []listPlayer {
 	return player
 }
 
+func setDiceNext(player []listPlayer) []listPlayer {
+	var playerRES []listPlayer
+	for i := 0; i < len(player); i++ {
+		p := toCharStr(i + 1)
+		var dice []int
+		for z := 0; z < len(player[i].Dice); z++ {
+			dice1 := rollDice()
+			dice = append(dice, dice1)
+		}
+		playerRES = append(playerRES, listPlayer{
+			Player: p,
+			Dice:   dice,
+			Score:  player[i].Score,
+		})
+	}
+	return playerRES
+}
+
 func evaluasi(in []listPlayer, score int) []listPlayer {
 	var (
-		Dadu6 []int
-		out   []listPlayer
-		count int
+		Dadu6     []int
+		DaduState []int
+		out       []listPlayer
+		count     int
 	)
 	for _, v := range in {
 		player := listPlayer{}
 		player.Player = v.Player
+		player.Score = v.Score
 		for _, z := range v.Dice {
 			if z == 6 {
 				Dadu6 = append(Dadu6, z)
+
 			} else {
 				player.Dice = append(player.Dice, z)
 			}
 		}
 		if count != 0 {
-			if len(Dadu6) != 0 {
-				for _, _ = range Dadu6 {
+			if len(DaduState) != 0 {
+				for _, _ = range DaduState {
 					player.Dice = append(player.Dice, 1)
 				}
-				player.Score = v.Score + 1
-				Dadu6 = []int{}
-			}
-		}
-
-		if count == (len(in) - 1) {
-			if len(Dadu6) != 0 {
-				for _, _ = range Dadu6 {
-					out[0].Dice = append(out[0].Dice, 1)
+				player.Score = player.Score + 1
+				DaduState = []int{}
+				if len(Dadu6) != 0 {
+					DaduState = Dadu6
+					Dadu6 = []int{}
 				}
-				out[0].Score = out[0].Score + 1
-				Dadu6 = []int{}
+				a := len(in) - 1
+				if count == (a) {
+					for _, _ = range DaduState {
+						out[0].Dice = append(out[0].Dice, 1)
+					}
+					out[0].Score = out[0].Score + 1
+					Dadu6 = []int{}
+					DaduState = Dadu6
+				}
 			}
+		} else {
+			DaduState = Dadu6
+			Dadu6 = []int{}
 		}
 		count++
 		out = append(out, player)
 	}
 	return out
+}
+
+func findWinner(in []listPlayer) bool {
+	var (
+		// win    bool
+		player []string
+	)
+
+	for _, v := range in {
+		if len(v.Dice) != 0 {
+			player = append(player, v.Player)
+		}
+	}
+	if len(player) == 1 {
+		fmt.Println("================================================")
+		fmt.Println("Winner = Player #", strings.TrimSpace(player[0]))
+		// fmt.Println()
+		return true
+	}
+	return false
 }
